a8f43269ca2d94b0c2ab329635b6237fd138c1f4
joaopedro9 <38812187+joaopedro9@users.noreply.github.com>
2023-11-04 19:25:45 -0300

    QUESTAO-01.c

--- QUESTAO-01/QUESTAO-01.c
+++ QUESTAO-01/QUESTAO-01.c
@@ -1,59 +1,27 @@
-const int pingPin = 6;
-long cemtimetros = 0;
+int ledpin = 7;
+int ldrpin = 5;
+int valor = 0;
+int percentual = 0;
 
-#include <LiquidCrystal_I2C.h>
-
-LiquidCrystal_I2C lcd(0x20,16,2);
-
-void setup() {
-  lcd.init();
-  lcd.clear();         
-  lcd.backlight();
-
-  lcd.setCursor(4,0);
-  lcd.print("Projeto");
-
-  lcd.setCursor(2,1);   
-  lcd.print("Trena Digital");
-
-  Serial.begin(9600);
+void setup(){
+	pinMode(7,OUTPUT);
+  	Serial.begin(9600);
 }
 
-void loop() {
-  long duration, cm;
+void loop(){
+	valor = analogRead(ldrpin);
+  	percentual = map(valor,0,1023,0,100);
+  if (percentual<40){
+  	digitalWrite(ledpin,1);
+    //delay(100);
+  	//digitalWrite(ledpin,0);
+    //delay(100);
 
-  pinMode(pingPin, OUTPUT);
-  digitalWrite(pingPin, LOW);
-
-  delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
-
-  delayMicroseconds(5);
-  digitalWrite(pingPin, LOW);
-
-  pinMode(pingPin, INPUT);
-  duration = pulseIn(pingPin, HIGH);
-
-  cm = microsecondsToCentimeters(duration);
-
-  if (cm != cemtimetros) {
-
-    cemtimetros = cm;
-
-    Serial.println(cm);
-
-    lcd.setCursor(2,0);
-    lcd.print("Medida em Cm");
-    lcd.setCursor(0,1);
-    lcd.print("                ");
-    lcd.setCursor(6,1);
-    lcd.print(cm);
-    lcd.print("cm");
-  }
-
-  delay(20);
-}
-
-long microsecondsToCentimeters(long microseconds) {
-  return microseconds / 29 / 2;
+  }else{
+    digitalWrite(ledpin,0);}
+  //Serial.println(valor);
+  Serial.print("Porcentagem de Luz na sala: ");
+  Serial.print(percentual);
+  Serial.println("%");
+  delay(500);
 }
